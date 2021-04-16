4393c7f49f2283d5fc43f87309ec7530ac849667
Phatchara Sangkeaw <sangkeaw_p@silpakorn.edu>
2021-04-17 06:42:40 +0700

    621310432
    621310432

---
+++ FoodLovers/app/src/main/java/th/ac/su/ict/foodlovers/data/Food.kt
@@ -0,0 +1,10 @@
+package th.ac.su.ict.foodlovers.data
+
+data class Food(
+    val imgFile:String,
+    val Food:String,
+    val Store:String,
+    val price:String,
+    val star:Int,
+    val data:String
+)
